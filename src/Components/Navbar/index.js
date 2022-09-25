c949159464e71df6a52727fab1e9b160e193a7f2
megha <meghamini40@gmail.com>
2022-09-25 13:44:58 +0530

    creating navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <nav className="flex justify-around py-4 bg-white/80
-            backdrop-blur-md shadow-md w-full
-            fixed top-0 left-0 right-0 z-10">
-                <div className="flex items-center">
-                </div>
+            <nav >
+                <ul>
+                    <li>home</li>
+                    <li>learn</li>
+                </ul>
             </nav>
         </div>
     )
