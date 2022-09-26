c774a9dea2b695fd1fbed7e0093a241ef00bd5d5
megha <meghamini40@gmail.com>
2022-09-26 23:20:47 +0530

    cards component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Cards from './Components/Cards';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Cards />
     </div>
   );
 }
