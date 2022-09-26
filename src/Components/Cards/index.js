8b9ab398a1ccef43e7a217f72eb45ca47940b760
megha <meghamini40@gmail.com>
2022-09-27 03:10:12 +0530

    render cards using map()

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -3,77 +3,21 @@ import card from './cards.json';
 const Cards = () => {
     return (
         <>
-            <div className='grid grid-cols-3 h-screen place-items-center'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  h-screen place-items-center max-w-[1250px] mx-auto'>
                 {card.map((item) => {
                     return (
-                        <div key={item.id} className='py-10'>
-                            <div className='w-72 rounded-lg overflow-hidden shadow-lg  hover:shadow-purple-500'>
-                                <img className='w-full h-3/4' src={item.image} alt={item.title} />
-                                <div className='px-6 py-4 h-1/4'>
+                        <div key={item.id} className="py-10  " >
+                            <div className='w-72 h-fit  object-cover rounded-lg overflow-hidden shadow-lg shadow-purple-500 '>
+                                <img className='w-full h-44' src={item.image} alt={item.title} />
+                                <div className='px-2 py-2 '>
                                     <div className='font-bold text-xl text-gray-800 mb-2'>{item.title}</div>
-                                    <p className='text-gray-700 text-base'>{item.description}</p>
+                                    <p className='text-gray-700 text-md'>{item.description}</p>
                                 </div>
                             </div>
                         </div>
                     )
                 })}
             </div>
-            {/* <div className='grid grid-cols-3 h-screen place-items-center'>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg overflow-hidden shadow-lg  hover:shadow-purple-500'>
-                        <img className='w-full h-3/4' src='./images/online-class.jpg' alt='online-classes' />
-                        <div className='px-6 py-4 h-1/4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Online Classes</div>
-                            <p className='text-gray-700 text-base'>Interact with your educator live</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg overflow-hidden shadow-lg  hover:shadow-purple-500'>
-                        <img className='w-full' src='./images/notations.jpg' alt='online-classes' />
-                        <div className='px-6 py-4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Notations and History</div>
-                            <p className='text-gray-700 text-base'>Interact with your educator live</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg overflow-hidden shadow-lg hover:shadow-purple-500'>
-                        <img className='w-full h-3/4' src='./images/flute-practice.jpg' alt='online-classes' />
-                        <div className='px-6 py-4 h-1/4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Practice</div>
-                            <p className='text-gray-700 text-base'>Interact with your educator live</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg overflow-hidden shadow-lg  hover:shadow-purple-500'>
-                        <img className='w-full h-3/4' src='https://ytimg.googleusercontent.com/vi/tb0gHAzpQPE/maxresdefault.jpg' alt='online-classes' />
-                        <div className='px-6 py-4 h-1/4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Fun Quiz</div>
-                            <p className='text-gray-700 text-base'>Enjoy fun quiz with learning</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg  shadow-lg  hover:shadow-purple-500'>
-                        <img className='w-full h-3/4' src='https://i0.wp.com/asmusicworld.com/wp-content/uploads/2020/08/12-e1538115961516.jpg?w=644ssl=1' alt='online-classes' />
-                        <div className='px-6 py-4 h-1/4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Indian Instrument</div>
-                            <p className='text-gray-700 text-base'>Enjoy fun quiz with learning</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-10'>
-                    <div className='w-72 rounded-lg  shadow-lg  hover:shadow-purple-500'>
-                        <img className='w-full h-3/4' src='https://i0.wp.com/asmusicworld.com/wp-content/uploads/2020/08/12-e1538115961516.jpg?w=644ssl=1' alt='online-classes' />
-                        <div className='px-6 py-4 h-1/4'>
-                            <div className='font-bold text-xl text-gray-800 mb-2'>Indian Instrument</div>
-                            <p className='text-gray-700 text-base'>Enjoy fun quiz with learning</p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </>
     )
 }
