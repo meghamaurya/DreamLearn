aaaa04e6bdc489b3d1e6af5a78d687bd1b928b5d
megha <meghamini40@gmail.com>
2022-09-27 12:54:44 +0530

    set routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Cards from './Components/Cards';
+import Home from './Components/HomePage';
+import Learn from './Components/LearnPage'
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Cards />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/learn' element={<Learn />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
