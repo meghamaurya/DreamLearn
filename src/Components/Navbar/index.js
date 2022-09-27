158221fdbff1ca4c12fa4f577db02ccdb9dc05d7
megha <meghamini40@gmail.com>
2022-09-27 16:05:40 +0530

    modified navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <div>
-            <nav className="flex bg-cyan-500 text-indigo-900">
-                <div className=' float-right m-3'>home</div>
-                <div className='float-right m-3'>learn</div>
+            <nav className="flex text-right justify-end bg-cyan-500 text-indigo-900">
+                <Link to="/" className='float-right m-3'>home</Link>
+                <Link to="/learn" className='float-right m-3'>learn</Link>
             </nav>
         </div>
     )
