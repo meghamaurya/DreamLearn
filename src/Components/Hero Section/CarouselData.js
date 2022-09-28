f9e44581c3a7a50073a5fdfabdd980304e1eac2e
dbsmandal <dipakmandaljio420@gmail.com>
2022-09-28 12:53:03 +0530

    create a carosel and login template

---
+++ src/Components/Hero Section/CarouselData.js
@@ -0,0 +1,19 @@
+import image1 from "./images/flute-practice.jpg"
+import image2 from "./images/notations.jpg"
+import image3 from "./images/online-class.jpg"
+export const CarouselData = [
+    {
+      image:image1,
+      imageid:1
+    },
+    {
+      image:image2,
+      imageid:2
+
+    },
+    {
+      image:image3,
+      imageid:3
+
+    }
+  ];
