e7d642083d9e7d634a3234bc602694a1e3412cfe
megha <meghamini40@gmail.com>
2022-09-29 22:43:46 +0530

    comment app css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './Components/Nav';
 import Footer from './Components/Footer';
