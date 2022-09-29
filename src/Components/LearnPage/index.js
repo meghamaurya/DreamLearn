056e71d4b7a433464cdba328557d562106de10ee
megha <meghamini40@gmail.com>
2022-09-29 13:46:23 +0530

    created demo classes

--- src/Components/LearnPage/index.js
+++ src/Components/LearnPage/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useEffect } from "react"
 import axios from 'axios'
-
+import ReactPlayer from 'react-player';
 const Learn = () => {
     const [demoClass, setDemoClass] = useState([]);
     useEffect(() => {
@@ -12,17 +12,22 @@ const Learn = () => {
             })
     }, [])
     return (
-        <div>
-            <h3>Demo Classes</h3>
-            {demoClass.map((data) => {
-                const { id, userName, bio, video } = data;
-                return (
-                    <div key={id}>
-                        <h2>{userName}</h2>
-                        <p>{bio}</p>
-                    </div>
-                )
-            })}
+        <div className="max-w-[1250px] mx-auto">
+            <h3 className='text-center mt-8 text-3xl'>Demo Classes</h3>
+            <div className=' grid grid-cols-3 m-4 mt-16'>
+                {demoClass.map((data) => {
+                    const { id, userName, bio, video } = data;
+                    return (
+                        <div key={id} className="inline-block w-80 h-56 overflow-hidden object-cover rounded-lg shadow-lg shadow-purple-500 mb-10" >
+                            <ReactPlayer controls url={video} width="100%" height="85%" />
+                            <div className='flex place-content-between text-gray-800 text-md items-center  m-1'>
+                                <h2>{userName}</h2>
+                                <p className=''>{bio}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
