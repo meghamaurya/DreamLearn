ef129a1215fe90458db25bda46fdd08ad3edd659
sourav <sauravkumarji99@gmail.com>
2022-09-29 14:08:27 +0530

    Modified Navbar and sticky footer done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Nav from './Component/Nav';
 import Footer from './Component/Footer';
+import Comp from './Component/Comp';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Footer />
+      <Comp />
     </div>
   );
 }
--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -2,33 +2,27 @@ import React from "react";
 
 function Nav() {
   return (
-    <div className="Nav">
-        <div>
-          <nav className="sticky top-0 flex px-6 py-4 bg-purple-900 text-white justify-between">
-            <div className="flex px-4 space-x-3">
+    <div className="sticky top-0 ">
+      <div className="container-fluid">
+        <nav className="flex  py-1 bg-purple-900 text-white justify-between">
+          <div className="flex px-0 -space-x-0.5 container-fluid">
             <img
               src="./MusicL.png"
-              className="h-16 rounded-lg cursor-pointer"
+              className="h-16 rounded-lg cursor-pointer mx-2  px-8"
               alt="Dream Learn Logo"
             ></img>
-            <span className="font-extrabold flex items-center justify-start text-3xl cursor-pointer">Dream Learn</span>
-            </div>
-            <div>
-            <ul className=" flex  sm:mt-0 items-center py-4 space-x-10 px-28 text-3xl ">
-              <li className="font-bold cursor-pointer mx-2  ">Home</li>
-              <li className="font-bold cursor-pointer">Learn</li>
-            </ul>
-            </div>
-          </nav>
-          <div>
-            <p>
-              This is my Paragraph
-            </p>
-            <p>
-              This is another paragraph
-            </p>
+            <span className="font-extrabold flex items-center justify-start text-3xl cursor-pointer">
+              Dream Learn
+            </span>
           </div>
-        </div>
+          <div>
+            <ul className=" flex  sm:mt-0 items-center py-4 space-x-10 px-20 text-3xl ">
+              <li className="font-bold cursor-pointer hover:text-purple-400">Home</li>
+              <li className="font-bold cursor-pointer ">Learn</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
