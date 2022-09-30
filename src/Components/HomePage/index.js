60f1c0e1a3a44a6c52ad1db539c90ccad57a35d5
megha <meghamini40@gmail.com>
2022-09-30 17:18:55 +0530

    call signup in app

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cards from '../Cards'
 import card from '../Cards/cards.json';
+import Signup from '../Auth/Signup';
 import Signin from '../SignIN/index'
 const Home = () => {
     return (
         <div>
+            <Signup />
             <Signin />
             <Cards cards={card} />
         </div>
