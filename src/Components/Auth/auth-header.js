7c3eb9cfaf70f988efac7456cf78d1068b7df6d2
megha <meghamini40@gmail.com>
2022-10-01 15:51:03 +0530

    create data service for user

---
+++ src/Components/Auth/auth-header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function authHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.accessToken) {
+        return { 'x-access-token': user.accessToken };
+    } else {
+        return {};
+    }
+}
