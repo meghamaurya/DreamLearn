dcc8913e7bf144b6a91b24ad0d19b7d4bf636c2e
megha <meghamini40@gmail.com>
2022-10-01 16:40:18 +0530

    implement jwt auth for signin

--- src/Components/SignIN/SignIn.js
+++ src/Components/SignIN/SignIn.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Carousel from "../Hero Section/Carousel";
+import AuthService from "../Auth/auth.service";
+
 function SignIn() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState('');
     const validationSchema = Yup.object().shape({
         username: Yup.string().required('**Email is required'),
         password: Yup.string().required('**Password is required')
@@ -12,6 +18,30 @@ function SignIn() {
 
     const onSubmit = data => {
         console.log(JSON.stringify(data, null, 2))
+        e.preventDefault();
+
+        setMessage('');
+        setLoading(true);
+
+        if (loginbtn.current.context._errors.length === 0) {
+            AuthService.login(username, password).then(
+                (response) => {
+                    console.log("successfully login", response)
+                    // Navigate("/learn");
+                    // window.location.reload();
+                },
+                (error) => {
+                    console.log(error)
+                    // const resMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+
+                    setLoading(false);
+                    // setMessage(resMessage);
+                }
+            );
+        }
+        else {
+            setLoading(false);
+        }
     };
 
     const {
@@ -29,7 +59,7 @@ function SignIn() {
 
             <div className="w-full mt-4 max-w-80% flex justify-start items-center pt-2">
                 <div className="w-full">
-                    <form className="bg-white shadow-md rounded px-6 pt-6 pb-1 mb-3" onSubmit={handleSubmit(onSubmit)}>
+                    <form className="bg-white shadow-md rounded px-6 pt-6 pb-1 mb-3" ref={form} onSubmit={handleSubmit(onSubmit)}>
                         <div className="flex flex-col items-center justify-center lg:justify-start">
                             <p className=" text-md mb-1 text-center text-purple-900 font-semibold">Sign in with</p>
                             <div className="flex">
@@ -86,6 +116,8 @@ function SignIn() {
                                 name="email"
                                 type="text"
                                 {...register('email')}
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
                                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'is-invalid' : ''}`}
                             />
                             <div className="text-red-600 font-semibold">{errors.email?.message}</div>
@@ -97,13 +129,28 @@ function SignIn() {
                                 name="password"
                                 type="password"
                                 {...register('password')}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.password ? 'is-invalid' : ''}`}
                             />
                             <div className="text-red-600 font-semibold">{errors.password?.message}</div>
                         </div>
+                        {message && (
+                            <div className="text-red-500">{message}</div>
+                        )}
 
                         <div className='form-group'>
-                            <button type='submit' className='btn bg-purple-900 text-white p-1 rounded-md shadow-md hover:bg-purple-500 hover:text-white hover:font-semibold shadow-purple-300 hover:shadow-purple-900 hover:shadow-md '>Sign In</button>
+                            <button type='submit' ref={loginbtn} disabled={loading} className='btn bg-purple-900 text-white p-1 rounded-md shadow-md hover:bg-purple-500 hover:text-white hover:font-semibold shadow-purple-300 hover:shadow-purple-900 hover:shadow-md '>
+                                {loading && (
+                                    <div class="text-center">
+                                        <div role="status">
+                                            <svg class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-purple-900" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                )}<span>Sign In</span></button>
                         </div>
                         <Link className="inline-block align-baseline text-sm text-purple-500 hover:text-purple-900" to="/SignUp">
                             You don't have an account?{" "}Sign Up
