f4c776eb1bbbf42d56ff553f505ba6a0e2d48d75
megha <meghamini40@gmail.com>
2022-10-02 11:11:18 +0530

    auth service user

--- src/Components/Auth/auth.service.js
+++ src/Components/Auth/auth.service.js
@@ -3,15 +3,16 @@ import axios from "axios";
 const API_URL = "";
 
 const signup = async (username, email, password) => {
-    const response = await axios.post(API_URL + "signup", {
+    await axios.post(API_URL + "signup", {
         username,
         email,
         password,
-    });
-    if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
-    }
-    return response.data;
+    }).then((response) => {
+        if (response.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+    })
 };
 
 const signin = async (username, password) => {
