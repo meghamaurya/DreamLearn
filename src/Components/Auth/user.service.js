e5c7caf46d9ad14f8ade4cecf891abcb0d4633e4
megha <meghamini40@gmail.com>
2022-10-04 15:55:02 +0530

    created data service

--- src/Components/Auth/user.service.js
+++ src/Components/Auth/user.service.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "";
+const API_URL = "https://dreamlearn-capstone.herokuapp.com/";
 
 const getLearnerDashBoard = () => {
-    return axios.get(API_URL, "user", { headers: authHeader() });
+    return axios.get(API_URL, "api/authorise/learner", { headers: authHeader() });
 }
-// const getEducatorDashBoard = () => {
-//     return axios.get(API_URL, "user", {headers: authHeader()});
-// }
-
-const LearnerService = {
-    getLearnerDashBoard
+const getEducatorDashBoard = () => {
+    return axios.get(API_URL, "api/authorise/educator", { headers: authHeader() });
 }
 
-export default LearnerService;
+const UserService = {
+    getLearnerDashBoard,
+    getEducatorDashBoard
+}
+
+export default UserService;
