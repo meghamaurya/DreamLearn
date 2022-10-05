1c8a69259702641ea729e5e6df4512feb725482b
megha <meghamini40@gmail.com>
2022-10-06 00:23:45 +0530

    created instrument detail page

--- src/Components/Instrument/index.js
+++ src/Components/Instrument/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function Instrument() {
     const [instruments, setInstruments] = useState({});
@@ -10,17 +10,18 @@ export default function Instrument() {
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${instrument}`)
             .then((res) => {
-                console.log(res.data)
+                console.log("courses", res.data)
                 setInstruments(res.data);
             })
     }, [instrument])
-    return (
-        <div>
-            <p>{instruments.title}</p>
-            <div>
-                <img className="w-72 h-72" src={instruments.image} alt={instrument.title} width="300px" />
-                <p>{instruments.price}</p>
-            </div>
+    return (<div >
+        <h3 className='text-center mt-10 text-4xl font-semibold text-purple-900'>{instruments.title} Courses</h3>
+        <div className="w-80 p-4 mt-14 inline-block cursor-pointer "><Link to={`/instruments/${instruments.id}/register`}>
+            {/* <div className="w-80 p-4 mt-14 inline-block cursor-pointer "><Link to={`/instruments/${instrument}`}> */}
+            <img className="w-full h-56 " src={instruments.image} alt={instruments.title} />
+            <h2 className="float-left pt-3 text-lg font-semibold">{instruments.category}</h2>
+            <p className="float-left ">{instruments.description}</p></Link>
         </div>
+    </div>
     )
 }
