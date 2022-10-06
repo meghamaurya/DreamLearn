a38471e2272c1b7b94d589096441c4f8bf27f338
megha <meghamini40@gmail.com>
2022-10-07 01:58:42 +0530

    adding loading and modified schedule css

--- src/Components/Schedule/index.js
+++ src/Components/Schedule/index.js
@@ -18,14 +18,14 @@ const Schedule = () => {
                 return (
                     <div className="flex w-8/12 mt-7 ml-64 justify-between border shadow-sm rounded-md shadow-purple-500">
                         <div className="flex flex-col">
-                            <div className="font-bold text-left p-2 text-2xl text-purple-900">Topics</div>
-                            <div className="p-2 font-semibold text-lg text-purple-700">{item.category}</div>
+                            <div className="font-bold text-left p-2 pb-0 text-2xl text-purple-900">Topics</div>
+                            <div className="p-2 pt-1 font-semibold text-lg text-purple-700">{item.category}</div>
                         </div>
                         <div>
-                            <div className=" pt-8 font-semibold text-lg text-purple-700">{item.price}</div>
+                            <div className=" pt-7 font-semibold text-lg text-purple-700">{item.price}</div>
                         </div>
                         <div className="">
-                            <button className="border p-1 m-2 pl-2 pr-2 mt-7 text-lg rounded-lg bg-purple-900 text-white" >Scheduled</button>
+                            <button className="border p-1 m-2 pl-2 pr-2 mt-6 text-lg rounded-lg bg-purple-900 text-white" >Scheduled</button>
                         </div>
                     </div>
                 )
