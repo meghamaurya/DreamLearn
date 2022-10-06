29e451db767de31c7cdc519ce89d15eed5e088a3
megha <meghamini40@gmail.com>
2022-10-06 17:29:59 +0530

    refactor code

--- src/Components/Auth/user.service.js
+++ src/Components/Auth/user.service.js
@@ -3,14 +3,19 @@ import authHeader from "./auth-header";
 
 const API_URL = "https://dreamlearn-capstone.herokuapp.com/";
 
+const getAllData = () => {
+    return axios.get(API_URL + "api/test/all");
+}
+
 const getLearnerDashBoard = () => {
-    return axios.get(API_URL, "api/authorise/learner", { headers: authHeader() });
+    return axios.get(API_URL + "api/authorise/learner", { headers: authHeader() });
 }
 const getEducatorDashBoard = () => {
-    return axios.get(API_URL, "api/authorise/educator", { headers: authHeader() });
+    return axios.get(API_URL + "api/authorise/educator", { headers: authHeader() });
 }
 
 const UserService = {
+    getAllData,
     getLearnerDashBoard,
     getEducatorDashBoard
 }
