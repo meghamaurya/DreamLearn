92e219122996d347d4371846036be841aaacf731
megha <meghamini40@gmail.com>
2022-10-10 11:34:45 +0530

    added success message

--- src/Components/Educator/DemoVideo/Form2.js
+++ src/Components/Educator/DemoVideo/Form2.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import EducatorService from '../../Auth/educator.service';
 const Form2 = (props) => {
-
+  const navigate = useNavigate();
   console.log(props, "videoInput")
-  // const [formSubmit, setFormSubmit] = useState(false);
+
   const [data, setData] = useState({
     title: "",
     instrument: "",
     video: null,
 
   });
+  const [formSubmit, setFormSubmit] = useState(false);
   console.log(data)
   function handleSubmit(e) {
     e.preventDefault();
-    // setFormSubmit(false);
+
+    setFormSubmit(false)
     let userDetail = new FormData();
     userDetail.append("title", data.title);
     userDetail.append("instrument", data.instrument);
@@ -21,14 +24,9 @@ const Form2 = (props) => {
     //for upload vide0 use only "demoVideo" key
     //for upload image use only "image" key
     EducatorService.uploadDemoVideo(userDetail);
-    // setTimeout(function () {
-    //   setFormSubmit(true);
-    // }, 5000);
-    // setTimeout(function () {
-    //   window.location.reload();
-    // }, 9000);
-
-
+    setTimeout(function () {
+      setFormSubmit(true);
+    }, 7000);
   }
 
   function handleChange(e) {
@@ -39,36 +37,45 @@ const Form2 = (props) => {
 
   }
 
+  const handleReload = () => {
+    window.location.reload();
+  }
   return (
     <div className=" mt-10 ">
       <form onSubmit={(e) => handleSubmit(e)} >
         <div className="mb-4">
-          <input type="text" placeholder='Video Title'
+          <input type="text" id="title" placeholder='Video Title'
             onChange={(e) => handleChange(e)} value={data.title}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
         </div>
         <div className="mb-4">
-          <select onChange={(e) => handleChange(e)} name="instrument"
+          <select onChange={(e) => handleChange(e)} name="instrument" id="instrument"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             <option >Select Instrument</option>
-            <option value="tabla"  >Tabla</option>
-            <option value="dholak" >Dholak</option>
-            <option value="Flute" >Flute</option>
-            <option value="veena">Veena</option>
-            <option value="harmonium" >Harmonium</option>
-            <option value="piano">Piano</option>
-            <option value="gitar" >Guitar</option>
-            <option value="drups" >Drums</option>
-            <option value="trumpet" >Trumpet</option>
-            <option value="violin">Violin</option>
+            <option value="tabla" id='tabla' >Tabla</option>
+            <option value="dholak" id='dholak'>Dholak</option>
+            <option value="Flute" id='Flute'>Flute</option>
+            <option value="veena" id='veena'>Veena</option>
+            <option value="harmonium" id='harmonium'>Harmonium</option>
+            <option value="piano" id='piano'>Piano</option>
+            <option value="gitar" id='gitar'>Gitar</option>
+            <option value="drups" id='drums'>Drums</option>
+            <option value="trumpet" id='trumpet'>Trumpet</option>
+            <option value="violin" id='violin'>Violin</option>
           </select>
         </div>
         <div className="mb-4">
           <button className="border p-1 mt-2 text-lg rounded-lg bg-purple-900 text-white w-20 m-auto focus:outline-none focus:shadow-outline" type='submit'>submit</button>
         </div>
-        {/* {formSubmit ? <div className='text-center mt-6 text-4xl font-semibold text-purple-900'>
-          Video Uploaded
-        </div> : null} */}
+        {formSubmit ? (
+          <>
+            <div className='text-center mt-6 text-4xl font-semibold text-purple-900'>
+              Video Uploaded
+            </div>
+            <div className="mb-4">
+              <button className="border p-1 mt-2 text-lg rounded-lg bg-purple-900 text-white w-20 m-auto focus:outline-none focus:shadow-outline" type='submit' onClick={handleReload}>Add More Video</button>
+            </div>
+          </>) : null}
 
       </form>
 
