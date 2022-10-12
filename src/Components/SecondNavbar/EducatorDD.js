2cc74fd400676b55dcd6db26b4a29552bd41aef7
megha <meghamini40@gmail.com>
2022-10-12 11:18:21 +0530

    added new tab mycourses

--- src/Components/SecondNavbar/EducatorDD.js
+++ src/Components/SecondNavbar/EducatorDD.js
@@ -3,27 +3,24 @@ import { useNavigate } from "react-router-dom"
 const EducatorDropdown = () => {
     const navigate = useNavigate()
 
-
-
-
     return (
         <div className='z-10 pl-24 mt-8 text-lg  text-purple-900 shadow-md shadow-purple-300'>
             <ul className="flex ">
-                <li className="relative pr-2">
-                    <button className="font-medium"
-                        onClick={() => { navigate("/adddemovideo") }}
-                    >Add Demo Video</button>
-
-
-                </li>
                 <li >
                     <button className="font-medium pl-2 pr-2"
                         onClick={() => { navigate("/addcourse") }}
                     >Add Course</button>
-
+                </li>
+                <li className="relative pr-2 pl-2">
+                    <button className="font-medium"
+                        onClick={() => { navigate("/adddemovideo") }}
+                    >Add Demo Video</button>
                 </li>
                 <li >
                     <button className="font-medium pl-2 pr-2" onClick={() => { navigate("/addschedule") }}>Add Schedule</button>
+                </li>
+                <li >
+                    <button className="font-medium pl-2 pr-2" onClick={() => { navigate("/mycourses") }}>My Courses</button>
                 </li>
             </ul>
         </div>
