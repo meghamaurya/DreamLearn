9beae52288039e64e6c1dc09642ffd981f7d1607
megha <meghamini40@gmail.com>
2022-10-14 02:30:45 +0530

    integrated with BE

--- src/Components/Courses/index.js
+++ src/Components/Courses/index.js
@@ -1,11 +1,9 @@
-import axios from "axios";
+
 import { useEffect, useState } from "react";
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import LearnerService from "../Auth/learner.service";
 const Courses = () => {
     const [courses, setCourses] = useState([]);
-    const params = useParams();
-    const { schedule } = params;
     useEffect(() => {
         LearnerService.registeredCourse()
             .then((res) => {
@@ -20,14 +18,14 @@ const Courses = () => {
         <div >
             <h3 className='text-center mt-10 text-4xl font-semibold underline underline-offset-2 text-purple-900'>My Courses</h3>
             {courses.map((course) => {
-
+                const { courseTitle } = course;
                 return (
                     <div className="flex w-8/12 mt-7 m-auto p-2 justify-between border shadow-sm rounded-md shadow-purple-500">
                         <div className="pr-6 ">
-                            <div className="font-bold text-left p-3 text-2xl text-purple-900">{course.courseTitle}</div>
+                            <div className="font-bold text-left p-3 text-2xl text-purple-900">{courseTitle}</div>
                         </div>
                         <div className="">
-                            <Link to={`/courses/${schedule}`} ><button className="border p-1 mt-7 pl-2 pr-3 text-lg rounded-lg bg-purple-900 text-white" >Schedule</button></Link>
+                            <button className="border p-1 mt-7 pl-2 pr-3 text-lg rounded-lg bg-purple-900 text-white" ><Link to={`/courses/${courseTitle}`} >Schedule</Link></button>
                         </div>
                     </div>
                 )
--- src/Components/Schedule/index.js
+++ src/Components/Schedule/index.js
@@ -1,29 +1,32 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import LearnerService from "../Auth/learner.service";
 
 const Schedule = () => {
     const [schedule, setSchedule] = useState([]);
-
+    const params = useParams();
+    const { courseTitle } = params;
     useEffect(() => {
-        axios.get('https://fakestoreapi.com/products?limit=5')
+        LearnerService.coursesSchedule(courseTitle)
             .then((res) => {
-                console.log("schedule", res.data);
-                setSchedule(res.data);
+                console.log("schedule", res.data.message);
+                setSchedule(res.data.message);
             })
     }, [])
     return (
         <>
             <h3 className='text-center mt-10 text-4xl font-semibold text-purple-900 underline underline-offset-2'>Schedule</h3>
-            {schedule.map((item) => {
+            {schedule.map((schedule) => {
+                const { date, slotEnd, slotStart, topic } = schedule;
                 return (
                     <div className="flex w-8/12 mt-7 m-auto justify-between border shadow-sm rounded-md shadow-purple-500">
                         <div className="flex flex-col">
                             <div className="font-bold text-left p-2 pb-0 text-2xl text-purple-900">Topics</div>
-                            <div className="p-2 pt-1 font-semibold text-lg text-purple-700">{item.category}</div>
+                            <div className="p-2 pt-1 font-semibold text-lg text-purple-700">{topic}</div>
                         </div>
                         <div>
-                            <div className=" pt-3 font-semibold text-lg text-purple-700">Date</div>
-                            <div className=" font-semibold text-lg text-purple-700">Time{item.price}</div>
+                            <div className=" pt-3 font-semibold text-lg text-purple-700">Date {date}</div>
+                            <div className=" font-semibold text-lg text-purple-700">Time {slotStart} - {slotEnd}</div>
                         </div>
                         <div className="">
                             <button className="border p-1 m-2 pl-2 pr-2 mt-6 text-lg rounded-lg bg-purple-900 text-white" >Scheduled</button>
