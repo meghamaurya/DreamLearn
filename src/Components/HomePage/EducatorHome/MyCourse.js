8f19c8bf5ad6bb1a5e873f3d4e15b81e1d7ca716
megha <meghamini40@gmail.com>
2022-10-18 00:04:03 +0530

    exchange educator demo page to courses

---
+++ src/Components/HomePage/EducatorHome/MyCourse.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CourseList from './CourseList'
+
+
+const EducatorHome = () => {
+  return (
+    <div className=' w-8/12 m-auto '>
+
+      <CourseList />
+
+    </div>
+  )
+}
+
+export default EducatorHome;
