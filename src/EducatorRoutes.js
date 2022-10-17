f8ba4407221ca851ed8f040ac80cee168d26364f
megha <meghamini40@gmail.com>
2022-10-18 00:14:40 +0530

    seperate educator routes file

---
+++ src/EducatorRoutes.js
@@ -0,0 +1,14 @@
+
+// function EducatorRoutes() {
+//     return (
+//         <>
+//             <Route path='/educator' element={<EducatorHome />} />
+//             <Route path='/adddemovideo' element={<AddDemoVideo />} />
+//             <Route path='/addcourse' element={<AddCourse />} />
+//             <Route path='/addschedule' element={<AddSchedule />} />
+//             <Route path='/educatorschedule/:title' element={<EducatorSchedule />} />
+//             <Route path='/mycourses' element={<MyCourses />} />
+//         </>
+//     )
+// }
+// // export default EducatorRoutes;
