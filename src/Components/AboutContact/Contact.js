31f51af3f01ac44fe1d8fea63f40a79b762c8d30
megha <meghamini40@gmail.com>
2022-11-14 13:36:05 +0530

    update contact details

--- src/Components/AboutContact/Contact.js
+++ src/Components/AboutContact/Contact.js
@@ -9,61 +9,59 @@ import { BsFillPersonCheckFill } from 'react-icons/bs'
 function Contact() {
   return (
     <>
-      <div className="w-8/12 m-auto py-10 pb-10  ">
+      <div className="w-8/12 m-auto py-5 pb-5  ">
         <h1 className="text-3xl px-20 opacity-4 shadow-md text-purple-900 inline-block">
           Contact Us
         </h1>
       </div>
-      <div className="  text-center w-8/12 m-auto mb-10 h-auto bg-gray-100 rounded-xl shadow-md overflow-hidden grid   ">
-        <div className="grid gap-20">
-          {
-            ContactData.map(({ id, github, linkden, name, portfolio, profileImage, role }) => {
-              return (
-                <div key={id} className='grid  bg-white rounded-xl shadow-md overflow-hidden '>
-                  <div className="grid grid-cols-2 mb-5 mt-5  ">
+      <div className="  text-center w-8/12 m-auto mb-4 h-auto mt-4 rounded-xl  overflow-hidden grid shadow-md gap-20  ">
+        {
+          ContactData.map(({ id, github, linkden, name, portfolio, role }) => {
+            return (
+              <div key={id} className='grid  rounded-xl shadow-md overflow-hidden '>
+                <div className="grid grid-cols-2 mb-3 mt-3  ">
 
-                    <div className=" self-center  ">
-                      <p className="text-2xl font-bold">{name}</p>
-                      <p className="text-sm font-semibold">({role})</p>
+                  <div className=" self-center  ">
+                    <p className="text-2xl font-bold text-purple-900 ">{name}</p>
+                    <p className="text-sm font-semibold text-purple-700">({role})</p>
 
+                  </div>
+                  <div className="  self-center ">
+                    <div className="grid grid-cols-3">
+                      <a href={github} target='_blank' rel="noreferrer">
+                        <div className="grid place-content-center place-items-center ">
+                          <BsGithub className="w-8 h-8 text-purple-900 hover:text-purple-700" />
+                          <p className="text-sm text-purple-900 font-semibold">Github </p>
+                        </div>
+                      </a>
+                      <a href={linkden} target='_blank' rel="noreferrer">
+                        <div className="grid place-content-center place-items-center  ">
+                          <AiFillLinkedin className="w-8 h-8 text-purple-900 hover:text-purple-700" />
+                          <p className="text-sm text-purple-900 font-semibold">Linkden </p>
+                        </div>
+                      </a>
+                      <a href={portfolio} target='_blank' rel="noreferrer">
+                        <div className="grid place-content-center place-items-center  ">
+                          <BsFillPersonCheckFill className="w-8 h-8 text-purple-900 hover:text-purple-700" />
+                          <p className="text-sm text-purple-900 font-semibold">Portfolio</p>
+                        </div>
+
+                      </a>
                     </div>
-                    <div className="  self-center ">
-                      <div className="grid grid-cols-3">
-                        <a href={github} target='_blank' rel="noreferrer">
-                          <div className="grid place-content-center ">
-                            <BsGithub className="w-12 h-12" />
-                            <p className="text-sm text-sky-500 font-semibold">Github </p>
-                          </div>
-                        </a>
-                        <a href={linkden} target='_blank' rel="noreferrer">
-                          <div className="grid place-content-center ">
-                            <AiFillLinkedin className="w-12 h-12" />
-                            <p className="text-sm text-sky-500 font-semibold">Linkden </p>
-                          </div>
-                        </a>
-                        <a href={portfolio} target='_blank' rel="noreferrer">
-                          <div className="grid place-content-center ">
-                            <BsFillPersonCheckFill className="w-12 h-12" />
-                            <p className="text-sm text-sky-500 font-semibold">Portfolio</p>
-                          </div>
-
-                        </a>
-                      </div>
-
-
-                    </div>
-
-
 
 
                   </div>
 
 
+
+
                 </div>
-              )
-            })
-          }
-        </div>
+
+
+              </div>
+            )
+          })
+        }
 
       </div>
 
